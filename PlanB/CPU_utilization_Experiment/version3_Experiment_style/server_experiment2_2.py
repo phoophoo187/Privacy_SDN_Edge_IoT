a47fd001d95b4772ff43781b611fdeb9cc3ce426
art013219748 <art013219748@hotmail.com>
2021-09-29 07:10:40 +0700

    Create experiment style3

---
+++ PlanB/CPU_utilization_Experiment/version3_Experiment_style/server_experiment2_2.py
@@ -0,0 +1,35 @@
+import socket
+import csv
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('10.0.0.10', 12345))
+sock.listen(5)
+table_header = ['edge_name','unix_timestamp','percentage_cpu_utilisation']
+f1 = open('./Experiment2_2/Edge1_CPU/edge1CPU_MTESTnotuse.csv', 'w')
+f2 = open('./Experiment2_2/Edge4_CPU/edge4CPU_MTESTnotuse.csv', 'w')
+writer1 = csv.writer(f1)
+writer2 = csv.writer(f2)
+writer1.writerow(table_header)
+writer2.writerow(table_header)
+while True:
+    conn, addr = sock.accept()
+    from_client = ''
+    while True:
+        encode_data = conn.recv(1024)
+        if not encode_data:
+            break
+        else:
+            data = encode_data.decode()
+            if 'edge1' in data:
+                formatted_data1 = data.split('$')
+                writer1.writerow(formatted_data1)
+                print(formatted_data1)
+            elif 'edge4' in data:
+                formatted_data2 = data.split('$')
+                writer2.writerow(formatted_data2)
+                print(formatted_data2) 
+            #writer.writerow([data])
+            #print(data)
+    conn.close()
+    print('Client disconnect')
+
